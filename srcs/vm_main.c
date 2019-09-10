46c89454eddfcfcd6abcac93f65be9225ed6c696
Benjamin CARLIER <bcarlier@e1r1p2.42.fr>
2019-09-10 17:28:54 +0200

    parsing args, in progress

--- srcs/vm_main.c
+++ srcs/vm_main.c
@@ -6,121 +6,121 @@
 /*   By: bcarlier <bcarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 19:02:24 by bcarlier          #+#    #+#             */
-/*   Updated: 2019/09/09 19:02:31 by bcarlier         ###   ########.fr       */
+/*   Updated: 2019/09/10 17:27:40 by bcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft/includes/libft.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-#define MAX_FILE_SIZE 1000000
-
-typedef	struct			t_listread
+typedef struct 		s_vm
 {
-	char 				*line;
-	struct	t_listread	*next;
-}						s_listread
+	size_t			cycle_to_dump;
+	int				player[4];
+}					t_vm;
 
-void	ft_lstread_free(char *line)
+int check_argc(int argc)
 {
-	if(line)
-		free(line);
-	line = 0;
-}
-
-void	ft_lstread_del(t_listread	*lst, void (*del)(char *))
-{
-	t_listread *temp;
-
-	if (!*alst)
-		return ;
-	del((*alst)->line);
-	temp = (*alst)->next;
-	free(*alst);
-	while (temp)
+	if (argc == 1)
 	{
-		(*alst) = temp;
-		del((*alst)->line);
-		temp = (*alst)->next;
-		free(*alst);
+		ft_printf("%susage%s: %s./corewar%s [%s-dump nbr_cycles%s] ",
+				FT_UNDER, FT_EOC, FT_BOLD, FT_EOC, FT_BOLD, FT_EOC);
+		ft_printf("[[%s-n number%s] %schampion1.cor%s] ...\n",
+				FT_BOLD, FT_EOC, FT_BOLD, FT_EOC);
+		return (-1);
 	}
-	*alst = 0;
+	return (0);
 }
 
-void	ft_lstread_new(void const *content, size_t content_size)
+int ft_isnb_str(char *str)
 {
-	t_listread	*elem;
+	int i;
 
-	if (!(elem = (t_list*)malloc(sizeof(*elem))))
-		return (0);
-	if (!content)
-		elem->line = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit((int)str[i]))
+			return (-1);
+		i += 1;;
+	}
+	return (0);
+}
+
+/*
+** Parse for cycle_to_dump value :
+** Check if argv[i + 1] exist and if it is a positive number.
+** Store into vm->cycle_to_dump if yes, else return -1.
+*/
+
+int parse_dump(int argc, char **argv, t_vm *vm, size_t *i)
+{
+	if ((*i + 1 < argc) && (ft_isnb_str(argv[*i + 1]) == 0))
+		vm->cycle_to_dump = ft_atozu(argv[++*i]);
+	else
+		return (-1);
+	return (0);
+}
+
+int	parse_n(int ac, char **av, t_vm *vm, size_t *i)
+{
+	char	*s;
+
+	if ((*i + 1 < ac) && (*i + 2 < ac) && ft_isnb_str(av[*i + 1]) == 0
+			&& (s = ft_strstr(av[*i + 2], ".cor")) && s != av[*i + 2] && !s[4])
+	{
+		// plus de check a faire sur le player (duplicate, si trop de player...)
+		// 4 lignes qui suivent doivent etre remplacees
+		if (!vm->player[0])
+			vm->player[0] = ft_atozu(av[*i + 1]);
+		else if (!vm->player[1])
+			vm->player[1] = ft_atozu(av[*i + 1]);
+		*i += 2;
+	}
 	else
 	{
-		if (!(elem->line = malloc(content_size)))
-		{
-			free(elem);
-			return (0);
-		}
-		ft_memcpy(elem->line, content, content_size);
+		//ft_printf("NOPE\n"); //debug
+		return (-1);
 	}
-	elem->next = 0;
-	return (elem);
+	return (0);
 }
 
-void	ft_lstread_insert(t_listread	**alst, t_listread	*new)
+int	parse_options(int ac, char **av, t_vm *vm)
 {
-	t_listread	tmp;
-
-	tmp.line = (*alst)->line;
-	tmp.next = (*alst)->next;
-	(*alst)->line = new->line;
-	(*alst)->next = new;
-	new->line = tmp.line;
-	new->next = tmp.next;
-}
-
-static	int		read_cor_file(t_listread	*file_read)
-{
-	int			ret;
-	int			file_size;
-	char		buf[4096+1]
-	t_listread	*tmp;
-
-	file_size = 0;
-	while (ret = read(STDIN_FILENO, buf, 4096) && ret != -1)
+	size_t	i;
+	char	*s;
+	
+	i = 1;
+	if (check_argc(ac) == -1)
+		return (-1);
+	while (i < ac && av[i])
 	{
-		buf[ret] = '\0';
-		// cas ou fichier > 1mb, on free la list et on return
-		if ((file_size += ret) > MAX_FILE_SIZE)
-		{
-			ft_lstread_del(&(file_read), ft_lstread_free);
+		if (!ft_strcmp(av[i], "-dump") && parse_dump(ac, av, vm, &i) == -1)
 			return (-1);
-		}
-		// 1er cas : file_read == NULL, on creer le premier noeud
-		// 2eme cas : file_read existe, on ajoute a la fin
-		if (!file_read)
-			if (!(file_read = ft_lstread_new((void *)buf, (size_t)ret)))
-				return (-1);
+		else if (!ft_strcmp(av[i], "-n") && parse_n(ac, av, vm, &i) == -1)
+			return (-1);
+		else if ((s = ft_strstr(av[i], ".cor")) && s != av[i] && !s[4])
+			;	
+			//parse_player(ac, av, &vm, &i); // TODO
 		else
-		{
-			if(!(tmp = ft_lstread_new((void *)buf, (size_t)ret)))
-				return (-1);
-			ft_lstread_insert(&(file_read), tmp);
-		}
+			;
+		i += 1;
 	}
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	void(argc);
-	void(argv);
-	t_listread	*file_read;
+	int i;
+	t_vm vm;
 
-	// A TESTER, not done yet
-	if (!(ret = read_cor_file(file_read)))
-		ft_dprintf(STDERR_FILENO, "ERROR\n");
-	else
-		ft_printf("%s", "Parsing file in list done\n");
-	return (0)
+	//verif valeurs du op.h n'ont pas ete modifies !
+	ft_bzero(&vm, sizeof(vm));
+	if (parse_options(argc, argv, &vm) == 0)
+	{
+		ft_printf("cycle_to_dump : |%zu|\n", vm.cycle_to_dump);
+		ft_printf("player_1_nb : |%zu|\n", vm.player[0]);
+		ft_printf("player_2_nb : |%zu|\n", vm.player[1]);
+	}
+	return (0);
 }
