51da4ddd7465b04cc6347e6aadcb27e011e757f0
Benjamin CARLIER <bcarlier@e1r2p10.42.fr>
2019-09-19 11:50:11 +0200

    ldi, lldi and sti done, to be verified (carry and implementation)

---
+++ srcs/op/op_ldi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_ldi.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcarlier <bcarlier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/19 10:19:51 by bcarlier          #+#    #+#             */
+/*   Updated: 2019/09/19 11:48:40 by bcarlier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "corewar.h"
+
+int		op_ldi_lldi(t_vm *vm, t_process *proc)
+{
+	int	tmp;
+	int	tmp2;
+	int	addr;
+
+	if (!(((proc->op.ocp & 0xFC) == 0x54) || ((proc->op.ocp & 0xFC) == 0x64)
+				|| ((proc->op.ocp & 0xFC) == 0x94)
+				|| ((proc->op.ocp & 0xFC) == 0xA4)
+				|| ((proc->op.ocp & 0xFC) == 0xD4)
+				|| ((proc->op.ocp & 0xFC) == 0xE4)))
+		return (FAILURE);
+	if (0 < proc->op.p[2] && proc->op.p[2] <= REG_NUMBER)
+		return (FAILURE);
+	if ((proc->op.ocp & 0xC0) == 0x40)
+	{
+		if (!(0 < proc->op.p[0] && proc->op.p[0] <= REG_NUMBER))
+			return (FAILURE);
+		tmp = proc->r[proc->op.p[0] - 1];
+	}
+	else
+		tmp = ((proc->op.ocp & 0xC0) == 0x80) ? proc->op.p[0] : proc->op.ind[0];
+	if ((proc->op.ocp & 0x30) == 0x10)
+	{
+		if (!(0 < proc->op.p[1] && proc->op.p[1] <= REG_NUMBER))
+			return (FAILURE);
+		tmp2 = proc->r[proc->op.p[1] - 1];
+	}
+	else
+		tmp2 = proc->op.p[1];
+	addr = proc->op.op == 10 : (tmp + tmp2) % IDX_MOD: (tmp + tmp2);
+	proc->r[proc->op.p[2] - 1] = load_from_ram(vm, proc->pc + addr, 4);
+	proc->carry = addr == 0 ? 1 : 0;
+	return (SUCCESS);
+}
---
+++ srcs/op/op_sti.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_sti.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcarlier <bcarlier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/19 11:03:27 by bcarlier          #+#    #+#             */
+/*   Updated: 2019/09/19 11:48:27 by bcarlier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "corewar.h"
+
+int		op_sti(t_vm *vm, t_process *proc)
+{
+	int	tmp;
+	int	tmp2;
+	int	addr;
+
+	if (!(((proc->op.ocp & 0xFC) == 0x54) || ((proc->op.ocp & 0xFC) == 0x64)
+				|| ((proc->op.ocp & 0xFC) == 0x74)
+				|| ((proc->op.ocp & 0xFC) == 0x58)
+				|| ((proc->op.ocp & 0xFC) == 0x68)
+				|| ((proc->op.ocp & 0xFC) == 0x78)))
+		return (FAILURE);
+	if (!(0 < proc->op.p[0] && proc->op.p[0] <= REG_NUMBER))
+			return (FAILURE);
+	if ((proc->op.ocp & 0x30) == 0x10)
+	{
+		if (!(0 < proc->op.p[1] && proc->op.p[1] <= REG_NUMBER))
+			return (FAILURE);
+		tmp = proc->r[proc->op.p[1] - 1];
+	}
+	else
+		tmp = ((proc->op.ocp & 0x30) == 0x20) ? proc->op.p[1] : proc->op.ind[1];
+	if ((proc->op.ocp & 0xC) == 0x4)
+	{
+		if (!(0 < proc->op.p[2] && proc->op.p[2] <= REG_NUMBER))
+			return (FAILURE);
+		tmp2 = proc->r[proc->op.p[2] - 1];
+	}
+	else
+		tmp2 = proc->op.p[2];
+	addr = (tmp + tmp2) % IDX_MOD;
+	write_in_ram(vm, addr, proc->r[proc->op.p[0] - 1]);
+	return (SUCCESS);
+}
