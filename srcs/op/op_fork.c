a61900f531a3311be30a2c0ac7905ff6c3ee601a
Augustin LOPEZ <augustin@augustins-macbook-air.home>
2019-09-20 15:12:29 +0200

    Quickfix: negative address on fork

--- srcs/op/op_fork.c
+++ srcs/op/op_fork.c
@@ -6,7 +6,7 @@
 /*   By: bcarlier <bcarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 16:09:01 by bcarlier          #+#    #+#             */
-/*   Updated: 2019/09/20 11:55:18 by aulopez          ###   ########.fr       */
+/*   Updated: 2019/09/20 15:12:10 by aulopez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int		op_fork_lfork(t_vm *vm, t_process *proc)
 	addr = proc->op.op == 12
 		? proc->pc + proc->op.p[0] % IDX_MOD
 		: proc->pc + proc->op.p[0];
+	if (addr < 0)
+		addr = MEM_SIZE + addr % MEM_SIZE;
 	if (create_process(vm, addr, proc->player_id) == FAILURE)
 	{
 		vm->err = errno;
