9a008d52c75fabfae4bc2372c108ce6c3c8885b5
Benjamin CARLIER <bcarlier@e1r2p10.42.fr>
2019-09-20 11:41:07 +0200

    modified aff, carefull about vm of zaz way of doing with wrong mask on aff

--- debug/aff_test.s
+++ debug/aff_test.s
@@ -2,22 +2,24 @@
 .comment ""
 
 test_1:
-ld 0, r2
-st r2, 100
-live %0
-zjmp %100
-
-# Should show Lol and not jump cause 
+# Should show Lol and not jump cause carry value is 0
 ld %76, r3
 ld %111, r4
 ld %108, r5
 aff r3
-# to slow display for zaz vm : fork %100
+# to slow display for zaz ===> vm : fork %100
 aff r4
-# to slow display for zaz vm : fork %100
+# to slow display for zaz ===> vm : fork %100
 aff r5
 zjmp %100
 
-# Should jump cause carry value is 1
+test_2:
+# Should not jump cause carry value is 0, aff don't modify carry
+ld 0, r2
 aff r2
 zjmp %100
+
+test_3:
+# Should jump cause carry value is 1, add modify carry to 1
+add r6, r6, r6
+zjmp %1000
--- srcs/op/op_aff.c
+++ srcs/op/op_aff.c
@@ -6,7 +6,7 @@
 /*   By: bcarlier <bcarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 11:50:49 by bcarlier          #+#    #+#             */
-/*   Updated: 2019/09/19 18:03:57 by bcarlier         ###   ########.fr       */
+/*   Updated: 2019/09/20 11:39:36 by bcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,8 @@
 
 int	op_aff(t_process *proc)
 {
-	if (0 < proc->op.p[0] && proc->op.p[0] <= REG_NUMBER)
+	if (!(0 < (proc->op.p[0] >> 8) && (proc->op.p[0] >> 8) <= REG_NUMBER))
 		return (FAILURE);
-	// Probleme de load, !! proc->op.p[0] ne contient pas le REG
-	// debug
-	ft_printf("proc->op.p[0] value : %d\n", proc->op.p[0]);
-	// end debug
-	ft_printf("Aff : |%c|\n", proc->r[proc->op.p[0] - 1] % 256);
-	proc->carry = (proc->r[proc->op.p[0] - 1] % 256 == 0) ? 1 : 0;
+	ft_printf("Aff : %c\n", proc->r[(proc->op.p[0] >> 8) - 1] % 256);
 	return (SUCCESS);
 }
